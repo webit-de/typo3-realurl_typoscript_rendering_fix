5480f94043e8584ea599253ed1867b4462d8b47b
Dan Untenzu <untenzu@webit.de>
2017-06-22 09:24:56 +0200

    [TASK] Raise version constraints to TYPO3 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			/*TYPO3 core & Fluid Powered TYPO3*/
-			'typo3' => '7.6.0-7.6.99',
+			'typo3' => '7.6.0-8.7.99',
 			'cms' => '',
 			'extbase' => '',
 			'realurl' => '',
